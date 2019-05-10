51a5b243a40535f2a8b7133deed8deae2a6f765f
yangyan <yangyanyj@chinamobile.com>
2019-05-10 17:07:54 +0800

    Add ARG and PKG_VERSION to Dockerfile
    Change-Id: I260ab6b5a2c9d7cb464649eda0ff96d1412e57af
    Issue-ID: VFC-1383
    Signed-off-by: yangyan <yangyanyj@chinamobile.com>

--- docker/docker-env-conf.sh
+++ docker/docker-env-conf.sh
@@ -8,7 +8,7 @@ install_sf(){
 
     # get binary zip from nexus - vfc-nfvo-genericparser
 
-    wget -q -O modeling-genericparser.zip 'https://nexus.onap.org/service/local/artifact/maven/redirect?r=snapshots&g=org.onap.modeling.genericparser&a=modeling-genericparser&e=zip&v=1.0.1-SNAPSHOT' && \
+    wget -q -O modeling-genericparser.zip 'https://nexus.onap.org/service/local/artifact/maven/redirect?r=snapshots&g=org.onap.modeling.genericparser&a=modeling-genericparser&e=zip&v=${pkg_version}-SNAPSHOT&e=zip' && \
     unzip modeling-genericparser.zip && \
     rm -rf modeling-genericparser.zip && \
     pip install --upgrade setuptools pip  && \
